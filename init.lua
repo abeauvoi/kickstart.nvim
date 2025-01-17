99265f116893894abe471a5a6826b7fa6a0d79f3
Alexandre Beauvois <abeauvoi@student.42.fr>
2025-01-18 00:32:57 +0100

    merge changes

--- init.lua
+++ init.lua
@@ -216,6 +216,31 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
+-- set shell based on system
+-- toggleterm-specific
+if vim.fn.has 'unix' == 1 and vim.fn.system 'uname' == 'Linux\n' then
+  if vim.fn.executable 'zsh' == 1 then
+    vim.opt.shell = 'zsh'
+  else
+    vim.opt.shell = 'bash'
+  end
+else
+  -- Set PowerShell as the default shell if available
+  if vim.fn.executable 'pwsh' == 1 then
+    vim.opt.shell = 'pwsh'
+  else
+    vim.opt.shell = 'powershell'
+  end
+
+  -- Set common shell options for PowerShell
+  vim.opt.shellcmdflag =
+    "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
+  vim.opt.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+  vim.opt.shellpipe = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+  vim.opt.shellquote = ''
+  vim.opt.shellxquote = ''
+end
+
 -- [[ Configure and install plugins ]]
 --
 --  To check the current status of your plugins, run
--- lua/custom/config/init.lua
+++ lua/custom/config/init.lua
@@ -1,3 +1,3 @@
-require 'custom.config.options'
 require 'custom.config.keymaps'
+require 'custom.config.options'
 require 'custom.config.autocmds'
